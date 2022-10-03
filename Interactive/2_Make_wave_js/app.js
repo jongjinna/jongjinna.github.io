615532958c3ea378ff41db29e009d242f3dd07c6
jongjinna <njjwa1004@naver.com>
2022-10-03 14:59:38 +0900

    :art: add interactive

---
+++ Interactive/2_Make_wave_js/app.js
@@ -0,0 +1,45 @@
+import {
+    Wavegroup
+} from "./wavegroup.js";
+
+class App{
+    constructor(){
+        this.canvas = document.createElement("canvas");
+        this.ctx = this.canvas.getContext("2d");
+
+        
+        this.wavegroup = new Wavegroup();
+        
+        document.body.appendChild(this.canvas);
+        window.addEventListener("resize",this.resize.bind(this),false);
+        this.resize();
+        
+        
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+        
+        this.canvas.width = this.stageWidth * 2;
+        this.canvas.height = this.stageHeight * 2;
+        this.ctx.scale(2, 2);
+
+        this.wavegroup.resize(this.stageWidth, this.stageHeight);
+    }
+
+    animate(t) {
+
+        this.ctx.clearRect(0,0,this.stageWidth,this.stageHeight);
+        
+        this.wavegroup.draw(this.ctx);
+    
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+}
+
+window.onload = () => {
+    new App();
+};
+
