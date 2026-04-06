a06ebc41c93cfe48c0ca70853fd9ffcf5165c671
jongjinna <njjwa1004@naver.com>
2026-04-06 22:54:58 +0900

    :art: add

---
+++ src/config.ts
@@ -0,0 +1,80 @@
+export const siteConfig = {
+  name: "Jongjin Na",
+  title: "Swap Sales Trader",
+  description: "Portfolio website of Jongjin Na",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "your-email@example.com",
+    linkedin: "https://linkedin.com/in/yourprofile",
+    // twitter: "https://x.com/rfitzio",
+    // github: "https://github.com/RyanFitzgerald",
+  },
+  aboutMe:
+    "I have been working in swap desk and execution at Samsung Securities for two years. Currently, I am responsible for handling inbound swaps, with a primary focus on middle-office operations.",
+  skills: ["Swap", "Equity", "Excution", "Python", "ddd", "sss"],
+  projects: [
+    // {
+    //   name: "AI Dev Roundup Newsletter",
+    //   description:
+    //     "One concise email. Five minutes. Every Tuesday. Essential AI news & trends, production-ready libraries, powerful AI tools, and real-world code examples",
+    //   link: "https://aidevroundup.com/?ref=devportfolio",
+    //   skills: ["React", "Node.js", "AWS"],
+    // },
+    // {
+    //   name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
+    //   description:
+    //     "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
+    //   link: "https://fullstackextensions.com/?ref=devportfolio",
+    //   skills: ["React", "Node.js", "AWS"],
+    // },
+    // {
+    //   name: "ExtensionKit",
+    //   description:
+    //     "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
+    //   link: "https://extensionkit.io/?ref=devportfolio",
+    //   skills: ["React", "Node.js", "AWS"],
+    // },
+  ],
+  experience: [
+    {
+      company: "Samsung Securities",
+      title: "Swap Junior",
+      dateRange: "Dec 2021 - Present",
+      bullets: [
+        "Mainly responsible for swap middle-office operations,  including trade execution and sales support.",
+        "Recently took charge of inbound swap transactions, ensuring smooth processing and accurate settlement.",
+        "Managed the development and maintenance of Swap Front System, utilizing Bloomberg API for real-time data integration.",
+      ],
+    },
+    {
+      company: "Moin",
+      title: "Frontend Developer",
+      dateRange: "Jun 2021 - Aug 2021",
+      bullets: [
+        "BDeveloped and maintained front-end components for the wholesale sales page, enhancing user experience for financial product presentations and client interactions.",
+        "Collaborated closely with backend developers, design, and customer experience (CX) teams to support the creation of customer-friendly pages",
+      ],
+    },
+    {
+      company: "ROKA",
+      title: "Financial Specialist",
+      dateRange: "Jan 2019 - Aug 2020",
+      bullets: [
+        "Managed the full contract lifecycle, including execution, fund processing, and follow-up management.",
+        "Maintained organized contract documentation and implemented process improvements.",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Sungkyunkwan Univ. ",
+      degree: "Business Administration",
+      dateRange: "2017 - 2022",
+      achievements: [
+        "Graduated Magna Cum Laude with 3.8 GPA",
+        "Dean's List all semesters",
+        "President of Computer Science Club",
+      ],
+    },
+  ],
+};
