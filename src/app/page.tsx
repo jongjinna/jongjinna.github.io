a0cfeb934d67151f9232ec170647c4da47467827
jongjinna <njjwa1004@naver.com>
2026-04-11 18:10:53 +0900

    :fire: all

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/page.tsx
-import { redirect } from 'next/navigation';
-
-export default function RootPage() {
-  // This will instantly move the user to /home 
-  // and send a 307 (Temporary Redirect) status code.
-  redirect('/home');
-}
